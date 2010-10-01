f83af65902f73bdbe52e5fd1aec144880cb85ae2
Rohit Yadav <rohityadav89@gmail.com>
2010-10-02 02:37:17 +0530

    contribs shell script to setup contribs folder for xcompiling for windows on linux

---
+++ contribs/contribs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+QT4_FILE="qt4-4.6.2-win32-bin.tar.bz2"
+QT4_URL="http://download.videolan.org/pub/videolan/contrib/qt4-4.6.2-win32-bin.tar.bz2"
+
+ROOT_FOLDER=`pwd`
+DOWNLOADS_FOLDER=$ROOT_FOLDER/downloads
+
+# Get the dependencies, aka VLC+Qt
+mkdir -p $DOWNLOADS_FOLDER
+cd $DOWNLOADS_FOLDER
+
+if [ ! -f $QT4_FILE ]; then
+    wget $QT4_URL ;
+else
+    echo "Qt4 OK";
+fi
+
+cd $ROOT_FOLDER
+
+# bin and dlls
+mkdir bin && mkdir include
+
+# Qt
+tar xvf $DOWNLOADS_FOLDER/$QT4_FILE -C . --strip-components=1
+cd include && ln -sf qt4/src && cd ..
