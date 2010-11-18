af9e3bb0fb797c62ac252196b49f2800f21ed51c
Rohit Yadav <rohityadav89@gmail.com>
2010-11-18 06:13:52 +0530

    CMake TS: Add files for translations

---
+++ ts/cmakeqt_fr_FR.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>CMakeQt</name>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="14"/>
+        <source>CMakeQt</source>
+        <translation>CMakeQt in French</translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="20"/>
+        <source>CMakeQt by Rohit Yadav
+%1
+Contact = %2
+Website = %3
+Hostname = %4
+OS = %5
+Version = %6
+Codename =  %7</source>
+        <translation>CMakeQt par Rohit Yadav
+%1
+Contact = %2
+Siteweb = %3
+Hostname = %4
+OS = %5
+Version = %6
+Nom de code =  %7</translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="52"/>
+        <source>Svg Test Button 2</source>
+        <translation>Svg Test Bouton 2</translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="41"/>
+        <source>Button 1</source>
+        <translation>Bouton 1</translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.cpp" line="21"/>
+        <source>This is some text in English language.</source>
+        <translation>Ceci est un texte en langue Français.</translation>
+    </message>
+</context>
+</TS>
---
+++ ts/template.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CMakeQt</name>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="14"/>
+        <source>CMakeQt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="20"/>
+        <source>CMakeQt by Rohit Yadav
+%1
+Contact = %2
+Website = %3
+Hostname = %4
+OS = %5
+Version = %6
+Codename =  %7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="41"/>
+        <source>Button 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.ui" line="52"/>
+        <source>Svg Test Button 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/CMakeQt.cpp" line="30"/>
+        <source>This is some text in English language.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
